d902c06b2c3d8a15802aa35b65708eba540e0fb7
wowzai <code.fun.dev@gmail.com>
2014-09-14 19:53:39 +0800

    打印阶梯

---
+++ exe010.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    for i := 0; i < 8; i++ {
+        for j := 0; j <= i; j++ {
+            fmt.Printf("■■")
+        }
+        fmt.Printf("☺\n")
+    }
+}
