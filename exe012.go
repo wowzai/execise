22a56873356cd37f06a3f951470a011f73e38324
wowzai <code.fun.dev@gmail.com>
2014-09-24 23:11:43 +0800

    水仙花数

---
+++ exe012.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i,j,k,m = 0,0,0,100
+	for m = 100; m <1000; m++ {
+		i = m / 100
+		j = m % 100 / 10
+		k = m % 10
+		if m == i*i*i + j*j*j + k*k*k {
+			fmt.Printf("m=%d\t",m)
+		}
+	}
+	fmt.Println()
+}
