e728d54cfa21c848cfc6655bfddaa98d07ef981b
wowzai <code.fun.dev@gmail.com>
2014-09-26 06:43:54 +0800

    统计字符个数

---
+++ exe016.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "bufio"
+  "os"
+  "fmt"
+)
+
+func main() {
+	var i,j,k,l int = 0,0,0,0
+	reader := bufio.NewReader(os.Stdin)
+	input,_ := reader.ReadString('\n')
+	for _, rune := range input {
+		switch {
+		case (rune >= 'A' && rune <= 'Z'):
+			i++
+		case (rune >= 'a' && rune <= 'z'):
+			i++
+		case (rune == ' ' || rune == '\t'):
+			j++
+		case (rune >= '0' && rune <= '9'):
+			k++
+		default:
+			l++
+		}
+	}
+	fmt.Printf("all in all: char=%d space=%d digit=%d others=%d",i,j,k,l)
+}
