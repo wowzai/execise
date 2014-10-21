9abe5389f027b0b86009486b2f38454a49239ac3
wowzai <code.fun.dev@gmail.com>
2014-10-22 07:40:48 +0800

    递归阶乘

---
+++ exe026.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func recJX(x int) int {
+  if x < 0 {
+  	fmt.Printf("您输入的值不符合规范！\n")
+  	return -1
+  }
+  if x > 1 {
+  	return x * recJX(x-1)
+  } else {
+  	return 1
+  }
+}
+
+func main() {
+	jx5 := recJX(5)
+	fmt.Printf("5! = %d\n", jx5)
+}
