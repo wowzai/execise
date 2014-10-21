a80f7d758a93835e13bf90bfeb9944169481a5cf
wowzai <code.fun.dev@gmail.com>
2014-10-22 07:29:01 +0800

    分数序列

---
+++ exe024.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i,j,sum  = 2.0,1.0,0.0
+	//var t = 0.0  //中间变量
+	for k := 0; k < 20; k++ {
+        sum += i/j
+        /*
+        t = j  // t当作中间变量缓存j的值
+        j = i
+        i += t
+        */
+        // golang中也可以换成如下形式而无需中间变量
+        i,j = i+j, i
+	}
+	fmt.Printf("sum is %f\n",sum)
+}
