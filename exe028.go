fe816b2263720d4f46555f70bc6e2715ca447814
wowzai <code.fun.dev@gmail.com>
2014-10-23 07:15:13 +0800

    退出年龄

---
+++ exe028.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func age(n int) int {
+	if n < 1 {
+		fmt.Printf("您输入的参数不符合规范\n")
+		return -1
+	}
+	if n == 1 {
+		return 10
+	} else {
+		return 2 + age(n - 1)
+	}
+}
+
+func main() {
+	age := age(5)
+	fmt.Printf("第五个人的年龄为%d\n",age)
+}
