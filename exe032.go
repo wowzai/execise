05df7c0d2302d293949cd6ec3a7abdcad7965e15
wowzai <code.fun.dev@gmail.com>
2014-10-24 07:54:25 +0800

    改变控制台字体颜色

---
+++ exe032.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "fmt"
+    "syscall"
+)
+
+type (
+	HANDLE uintptr
+	WORD   uint16
+	DWORD  uint32
+)
+
+const (
+	STD_OUTPUT_HANDLE    = 0xFFFFFFF5
+    FOREGROUND_BLUE      = 0x01
+    FOREGROUND_GREEN     = 0x02
+    FOREGROUND_RED       = 0x04
+    FOREGROUND_INTENSITY = 0x08
+    BACKGROUND_BLUE      = 0x10
+    BACKGROUND_GREEN     = 0x20
+    BACKGROUND_RED       = 0x40
+    BACKGROUND_INTENSITY = 0x80
+)
+
+var (
+    modkernel32                 = syscall.NewLazyDLL("kernel32.dll")
+    procGetStdHandle            = modkernel32.NewProc("GetStdHandle")
+    procSetConsoleTextAttribute = modkernel32.NewProc("SetConsoleTextAttribute")
+)
+
+func GetStdHandle(nStdHandle DWORD) HANDLE {
+    ret, _, _ := procGetStdHandle.Call(uintptr(nStdHandle))
+    return HANDLE(ret)
+}
+
+func SetConsoleTextAttribute(hConsoleOutput HANDLE, wAttributes WORD) bool {
+    ret, _, _ := procSetConsoleTextAttribute.Call(
+        uintptr(hConsoleOutput),
+        uintptr(wAttributes))
+    return ret != 0
+}
+
+func textbackground(color int) {
+    hOut := GetStdHandle(STD_OUTPUT_HANDLE)
+    SetConsoleTextAttribute(hOut, WORD(color))
+}
+
+func main() {
+    for color := 0; color < 8; color++ {
+        textbackground(color)
+        fmt.Printf("This is color %d\n", color)
+        fmt.Printf("Press any key to continue\n")
+        fmt.Scanln()
+    }
+}
