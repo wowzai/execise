35058e42b261b178646fddaf27f4aab459342211
wowzai <code.fun.dev@gmail.com>
2014-10-25 15:41:51 +0800

    哎，我是SB

---
+++ exe034.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func noresult() {
+	fmt.Println("没有返回值的函数")
+}
+
+func oneresult() int {
+	fmt.Println("存在1个返回值的函数")
+	return 1
+}
+
+func tworesult() (int,bool) {
+	fmt.Println("存在2个返回值的函数")
+	return 2,true
+}
+
+func threeresult() (int,bool,bool){
+	fmt.Println("存在3个返回值的函数")
+	return 3,false,true
+}
+
+func main() {
+	noresult()
+	ret := oneresult()
+	fmt.Println("存在1个返回值的函数",ret)
+	_,b := tworesult()
+	fmt.Println("存在2个返回值的函数",b)
+	_,_,_ = threeresult()
+}
